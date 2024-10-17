58a65e7f6742016c801772d6932123ccda56cc6c
xueguichen <long1234@MG>
2024-10-17 18:58:20 +0800

    read

--- do-experiment-web/src/main/java/com/experiment/web/controller/RequestMappingHandlerController.java
+++ do-experiment-web/src/main/java/com/experiment/web/controller/RequestMappingHandlerController.java
@@ -2,6 +2,7 @@ package com.experiment.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author chenxuegui
@@ -12,10 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
    spring有四种处理器
    1、实现了Controller接口的bean对象，spring早期古老的处理器，返回页面视图       -- BeanNameUrlHandlerMapping 负责检测保存映射关系 -->  SimpleControllerHandlerAdapter 负责转发执行
    2、实现了HttpRequestHandler接口的bean对象，spring早期古老的处理器，返回数据  -- BeanNameUrlHandlerMapping 负责检测保存映射关系 -->  HttpRequestHandlerAdapter 负责转发执行
-   3、添加了 @RequestMapping 的方法
+   3、添加了 @RequestMapping 的方法  --- RequestMappingHandlerMapping 负责检测保存映射关系 -->  RequestMappingHandlerAdapter 负责转发执行
 * */
 @Controller
 public class RequestMappingHandlerController {
+
+	@RequestMapping("/requestMappingByParam")
+	public String requestMappingByParam(@RequestParam(value = "name") String name){
+		return "name";
+	}
 
 	@RequestMapping("/requestMappingByNone")
 	public String requestMappingByNone(String name){
