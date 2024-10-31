c11a87f57225fa88cabf3d893bbfc876c17371c5
xueguichen <long1234@MG>
2024-10-31 16:58:40 +0800

    read

--- do-experiment-web/src/main/java/com/experiment/web/spi/MyWebMvcConfigurer.java
+++ do-experiment-web/src/main/java/com/experiment/web/spi/MyWebMvcConfigurer.java
@@ -13,12 +13,13 @@ import java.util.List;
  * @since 2024/10/31
  */
 @Component
-@EnableWebMvc /* 自定义mvc --> 导入DelegatingWebMvcConfiguration --> 使用@Bean实例化mvc相关Bean（DispatcherServlet.properties类似） */
+@EnableWebMvc /* 自定义mvc --> 导入 DelegatingWebMvcConfiguration --> 使用@Bean实例化mvc相关Bean（DispatcherServlet.properties类似） */
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
 
 	/* 自定义数据类型转换器*/
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-		converters.add(new MappingJackson2HttpMessageConverter());/* Content-Type = application/json 数据类型转换器 */
+		//不需要手动添加，DelegatingWebMvcConfiguration中已经自动探测
+		//converters.add(new MappingJackson2HttpMessageConverter());/* 支持Content-Type = application/json 数据类型转换器 */
 	}
 }
