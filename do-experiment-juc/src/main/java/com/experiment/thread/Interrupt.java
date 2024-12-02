4bd7ce8f0c4e26c609ee143d5a0bf1da38362819
xueguichen <long1234@MG>
2024-12-02 19:01:34 +0800

    read

--- do-experiment-juc/src/main/java/com/experiment/thread/Interrupt.java
+++ do-experiment-juc/src/main/java/com/experiment/thread/Interrupt.java
@@ -1,5 +1,7 @@
 package com.experiment.thread;
 
+import org.junit.Test;
+
 /**
  * @author chenxuegui
  * @since 2024/12/2
@@ -9,7 +11,7 @@ public class Interrupt {
 	private final static Object lock1 = new Object();
 	private final static Object lock2 = new Object();
 
-	public static void doLockInterrupt(){
+	public static void interruptSyncronized(){
 		synchronized (lock1){
 			LogUtil.log(Thread.currentThread().getName() + " 获锁成功");
 			SleepUtil.sleep(5000);
@@ -17,12 +19,13 @@ public class Interrupt {
 		LogUtil.log(Thread.currentThread().getName() +"  释放锁");
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public  void testInterruptSyncronized(){
 		Thread t1 = new Thread("t1"){
 			@Override
 			public void run() {
 				LogUtil.log("t1 run - 等待获锁");
-				doLockInterrupt();
+				interruptSyncronized();
 			}
 		};
 
@@ -30,7 +33,7 @@ public class Interrupt {
 			@Override
 			public void run() {
 				LogUtil.log("t2 run - 等待获锁");
-				doLockInterrupt();
+				interruptSyncronized();
 			}
 		};
 		t1.start();
@@ -41,7 +44,7 @@ public class Interrupt {
 		SleepUtil.sleep(200);
 		t2.interrupt();              /* synchronized等待获锁时，不能响应中断  */
 		LogUtil.log("t2 run - 发起中断");
-
 	}
+
 
 }
