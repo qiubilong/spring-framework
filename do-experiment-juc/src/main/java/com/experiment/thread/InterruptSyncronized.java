22be2f566c84a5fecad10b611b7913cdd82700e7
xueguichen <long1234@MG>
2024-12-02 19:13:24 +0800

    read

--- do-experiment-juc/src/main/java/com/experiment/thread/InterruptSyncronized.java
+++ do-experiment-juc/src/main/java/com/experiment/thread/InterruptSyncronized.java
@@ -1,26 +1,27 @@
 package com.experiment.thread;
 
+
 import org.junit.Test;
 
 /**
  * @author chenxuegui
  * @since 2024/12/2
  */
-public class Interrupt {
+public class InterruptSyncronized {
 
 	private final static Object lock1 = new Object();
 	private final static Object lock2 = new Object();
 
 	public static void interruptSyncronized(){
-		synchronized (lock1){
+		synchronized (lock1){  /* synchronized等待获锁时，不能响应中断  */
 			LogUtil.log(Thread.currentThread().getName() + " 获锁成功");
 			SleepUtil.sleep(5000);
 		}
 		LogUtil.log(Thread.currentThread().getName() +"  释放锁");
 	}
 
-	public static void testInterruptSyncronized(){
-		Thread t1 = new Thread("t1"){
+	public static void main(String[] args) {
+		Thread t1 = new Thread("t1") {
 			@Override
 			public void run() {
 				LogUtil.log("t1 run - 等待获锁");
@@ -28,7 +29,7 @@ public class Interrupt {
 			}
 		};
 
-		Thread t2 = new Thread("t2"){
+		Thread t2 = new Thread("t2") {
 			@Override
 			public void run() {
 				LogUtil.log("t2 run - 等待获锁");
@@ -43,11 +44,40 @@ public class Interrupt {
 		SleepUtil.sleep(200);
 		t2.interrupt();              /* synchronized等待获锁时，不能响应中断  */
 		LogUtil.log("t2 run - 发起中断");
+
 	}
 
-
-	public static void main(String[] args) {
-		testInterruptSyncronized();
+	public static void lockMulti(Object lock1, Object lock2){
+		synchronized (lock1){
+			LogUtil.log("获锁lock1");
+			SleepUtil.sleep(200);
+			synchronized (lock2){
+				LogUtil.log("获锁lock2");
+				SleepUtil.sleep(200);
+			}
+		}
 	}
 
+	@Test
+	public void TestInterrupt_DeadLock(){
+
+		Thread t1 = new Thread("t1") {
+			@Override
+			public void run() {
+				lockMulti(lock2,lock1);
+			}
+		};
+
+		Thread t2 = new Thread("t2") {
+			@Override
+			public void run() {
+				lockMulti(lock1,lock2);
+			}
+		};
+
+		t1.start();
+		t2.start();
+		SleepUtil.sleep(200);
+		t2.interrupt();              /* synchronized等待获锁时，不能响应中断  */
+	}
 }
---
+++ do-experiment-juc/src/main/java/com/experiment/thread/Interrupt_DeadLock.java
@@ -0,0 +1,44 @@
+package com.experiment.thread;
+
+
+/**
+ * @author chenxuegui
+ * @since 2024/12/2
+ */
+public class Interrupt_DeadLock {  /* synchronized死锁时，不能响应中断  */
+	private final static Object lock1 = new Object();
+	private final static Object lock2 = new Object();
+
+	public static void lockMulti(Object lock1, Object lock2){ /* 发生死锁，可用 jstack -pid 查看 */
+		synchronized (lock1){
+			LogUtil.log("获锁lock1="+lock1);
+			SleepUtil.sleep(200);
+			synchronized (lock2){
+				LogUtil.log("获锁lock2="+lock2);
+				SleepUtil.sleep(200);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		Thread t1 = new Thread("t1") {
+			@Override
+			public void run() {
+				lockMulti(lock2,lock1);
+			}
+		};
+
+		Thread t2 = new Thread("t2") {
+			@Override
+			public void run() {
+				lockMulti(lock1,lock2);
+			}
+		};
+
+		t1.start();
+		t2.start();
+		SleepUtil.sleep(200);
+		t2.interrupt();              /* synchronized死锁时，不能响应中断  */
+	}
+
+}
