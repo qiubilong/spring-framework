0717da7d3ae7fabe3e0472a99303154c7be404d6
xueguichen <long1234@MG>
2024-12-06 19:33:31 +0800

    read

--- do-experiment-juc/src/main/java/com/experiment/thread/ThreadLocal_内存泄漏.java
+++ do-experiment-juc/src/main/java/com/experiment/thread/ThreadLocal_内存泄漏.java
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class ThreadLocal_内存泄漏 {
 
-	static ThreadLocal<byte[]> bigThreadLocal = new ThreadLocal<>(); /* 静态类ThreadLocal对象因为有强引用，就算是弱引用，也不会被GC回收 */
+	//static ThreadLocal<byte[]> bigThreadLocal = new ThreadLocal<>(); /* 静态类ThreadLocal对象因为有强引用，就算是弱引用，也不会被GC回收。如果没有remove也只是造成少量的内存泄漏 */
 
 	public static void main(String[] args) {
 
@@ -26,15 +26,19 @@ public class ThreadLocal_内存泄漏 {
 				public void run() {
 					/*
 					 * Thread.ThreadLocalMap.Entry = ThreadLocal对象 + value
-					 * ThreadLocal对象 被包装成 弱引用WeakReference，方法栈帧销毁后，没有强引用指向，被gc回收。
+					 * 局部ThreadLocal对象 被包装成 弱引用WeakReference，方法栈帧销毁后，没有强引用指向，被gc回收。
 					 *  但 线程池中的线程不会销毁，强引用着Thread.ThreadLocalMap.Entry.value，无法回收造成内存泄漏 */
 					ThreadLocal<byte[]> bigThreadLocal = new ThreadLocal<>();
+
+					//ThreadLocal一定要生成静态变量，否则Thread.ThreadLocalMap.Entry中的越来越多
 
 					byte[] value = new byte[5 * 1024 * 1024];//5M
 					bigThreadLocal.set(value);
 
 					SleepUtil.sleep(200);
 					Log.info("run end");
+
+					bigThreadLocal.remove();//删除私有变量，回收内存
 				}
 			});
 		}
