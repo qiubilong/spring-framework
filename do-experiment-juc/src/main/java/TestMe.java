261f814a1391a7151c861837c0f7b7caac1586c0
qiubilong <xueguichen@qq.com>
2024-12-09 08:53:37 +0800

    read

--- do-experiment-juc/src/main/java/TestMe.java
+++ do-experiment-juc/src/main/java/TestMe.java
@@ -3,8 +3,10 @@
  * @since 2024/11/28
  */
 public class TestMe {
+	static final int SHARED_SHIFT   = 16;
+	static final int SHARED_UNIT    = (1 << SHARED_SHIFT);  /* 2^16 */
 
 	public static void main(String[] args) {
-		System.out.println("xxxx");
+		System.out.println(SHARED_UNIT);
 	}
 }
--- do-experiment-juc/src/main/java/cn/tulingxueyuan/base/vola/VolatileCase.java
+++ do-experiment-juc/src/main/java/cn/tulingxueyuan/base/vola/VolatileCase.java
@@ -4,10 +4,18 @@ import cn.tulingxueyuan.tools.SleepTools;
 
 /**
  * 类说明：演示Volatile的提供的可见性
+ *
+ *
+ *               -------可见性 - volatile
+ *  synchronized
+ *               --------原子性
+ *
+ *    volatile是最轻量的线程通信机制，只适合一个线程写
+ *    volatile能做到的事情，synchronized都能做到
  */
 /* volatile声明的变量存在主存，而非cpu缓存 */
 public class VolatileCase {
-    private static boolean ready;
+    private volatile static boolean ready;
     private static int number;
 
     private static class PrintThread extends Thread{
@@ -15,7 +23,7 @@ public class VolatileCase {
         public void run() {
             System.out.println("PrintThread is running.......");
             while(!ready){
-                System.out.println("lll");
+                //System.out.println("lll");
             };//无限循环
             System.out.println("PrintThread end. number = "+number);
         }
@@ -25,7 +33,7 @@ public class VolatileCase {
         new PrintThread().start();
         SleepTools.second(1);
         number = 51;
-        ready = true;
+        ready = true; /* volatile写屏障,把目前所有的修改刷到主存 */
         SleepTools.second(5);
         System.out.println("main is ended!");
     }
