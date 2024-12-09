6fcbd12e689e274efb5358d991e127acaad830b1
xueguichen <long1234@MG>
2024-12-09 16:33:41 +0800

    read

--- do-experiment-juc/src/main/java/cn/tulingxueyuan/base/wait_notify/Express.java
+++ do-experiment-juc/src/main/java/cn/tulingxueyuan/base/wait_notify/Express.java
@@ -20,7 +20,7 @@ public class Express {
     public void change(){
         if (km < TOTAL){
             km = km +100;
-            System.out.println("the Km is "+this.km);
+            System.out.println("快递物流更新 Km is "+this.km);
         }
         if(km >= TOTAL){
             site = DIST_CITY;
@@ -30,13 +30,11 @@ public class Express {
 
     /*线程等待公里的变化*/
     public synchronized void waitKm(){
-        while(this.km <= TOTAL){
+        while(this.km < TOTAL){
             try {
                 wait();
-                System.out.println("Map thread["
-                                +Thread.currentThread().getId()
-                        +"] wake,I will change db");
-            } catch (InterruptedException e) {
+                System.out.println("地图线程 [" +Thread.currentThread().getName() +"] wake,I will change db");
+			} catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
@@ -47,12 +45,11 @@ public class Express {
         while(!this.site.equals(DIST_CITY)){
             try {
                 wait();
-                System.out.println("Notice User thread["+Thread.currentThread().getId()
-                		+"] wake");
-            } catch (InterruptedException e) {
+                System.out.println("通知用户线程 ["+Thread.currentThread().getName() +"] wake");
+			} catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println("the site is "+this.site+",I will call user");
+        System.out.println("快递到达目的地 "+this.site+",I will call user");
     }
 }
--- do-experiment-juc/src/main/java/cn/tulingxueyuan/base/wait_notify/TestExpress.java
+++ do-experiment-juc/src/main/java/cn/tulingxueyuan/base/wait_notify/TestExpress.java
@@ -5,12 +5,17 @@ import cn.tulingxueyuan.tools.SleepTools;
 /**
  *类说明：测试wait/notify/notifyAll
  */
-public class TestWN {
-    private static Express express = new Express(0,"WUHAN");
+public class TestExpress {
+    private static final Express express = new Express(0,"WUHAN");
 
     /*检查里程数变化的线程,不满足条件，线程一直等待*/
     private static class CheckKm extends Thread{
-        @Override
+
+		public CheckKm(String name) {
+			super(name);
+		}
+
+		@Override
         public void run() {
         	express.waitKm();
         }
@@ -18,27 +23,36 @@ public class TestWN {
 
     /*检查地点变化的线程,不满足条件，线程一直等待*/
     private static class CheckSite extends Thread{
-        @Override
+
+		public CheckSite(String name) {
+			super(name);
+		}
+
+		@Override
         public void run() {
         	express.waitSite();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        for(int i=0;i<2;i++){
-            new CheckSite().start();
+        for(int i=0;i<1;i++){
+            new CheckSite("快递目的地检查").start();
         }
-        for(int i=0;i<2;i++){
-            new CheckKm().start();
+        for(int i=0;i<1;i++){
+            new CheckKm("快递公里数记录").start();
         }
         SleepTools.ms(500);
 
         for(int i=0; i<5; i++){
             synchronized (express){
                 express.change();
-                express.notify();
+
+				/* notify只能唤醒一个线程，如果存在处理业务不同的多个线程等待条件，将会导致唤醒丢失，线程永远挂起无法结束。因此总是建议使用notifyAll() */
+                //express.notify();
+				express.notifyAll();
             }
-            SleepTools.ms(500);
-        }
+            SleepTools.second(3);
+			System.out.println("-------------------------------");
+		}
     }
 }
