9d97c6206289e3f734c1b7e5a60371ea4e85d19a
xueguichen <long1234@MG>
2025-05-09 19:28:08 +0800

    springboot

--- do-experiment-boot/src/main/java/com/experiment/web/AppMain.java
+++ do-experiment-boot/src/main/java/com/experiment/web/AppMain.java
@@ -1,18 +1,13 @@
 package com.experiment.web;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.TypeExcludeFilter;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author chenxuegui
  * @since 2025/2/6
  */
-@EnableAutoConfiguration
-@ComponentScan(excludeFilters = { @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class), @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class) })
+@SpringBootApplication
 public class AppMain {
 
 	public static void main(String[] args) {
